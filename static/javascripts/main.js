e2ec397ca144fa1ddcb79a955c9df8e5762dc9ee
Muhan Guclu <magicmuhan@gmail.com>
2018-06-14 16:09:47 -0400

    work history, first idea for work section layout

--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
 
+  var workSelectedClass = 'work-selected';
+  var workInactiveClass = 'work-inactive';
+  var workDefaultClass = 'work-default';
+
   function animateSkills() {
     setTimeout(function () {
       if ($('#about-wrapper').length) {
@@ -8,5 +12,34 @@ $(document).ready(function () {
     }, 500)
   }
 
+  function clearWorkSelection() {
+    $('#work-section-jobs').removeClass(workSelectedClass);
+    $('#work-section-projects').removeClass(workSelectedClass);
+    $('#work-section-jobs').removeClass(workInactiveClass);
+    $('#work-section-projects').removeClass(workInactiveClass);
+    $('#work-section-jobs').removeClass(workDefaultClass);
+    $('#work-section-projects').removeClass(workDefaultClass);
+  }
+
+  $.fn.selectSection = function() {
+    clearWorkSelection();
+
+    this.addClass(workSelectedClass)
+
+    if (this[0].id == 'work-section-jobs') {
+      $('#work-section-projects').addClass(workInactiveClass); 
+    } else {
+      $('#work-section-jobs').addClass(workInactiveClass); 
+    }
+  }
+
   animateSkills();
+
+  $("#work-section-jobs.work-inactive h1, #work-section-jobs.work-default h1").on("click", function (){
+    console.log('click')
+    $("#work-section-jobs").selectSection(); 
+  });
+  $("#work-section-projects.work-inactive h1, #work-section-projects.work-default h1").on("click", function (){
+    $("#work-section-projects").selectSection(); 
+  });
 });
